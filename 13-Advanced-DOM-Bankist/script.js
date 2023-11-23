36e4769b0ce883df094a538fd9589c6a9be461e5
Maurício <itoalberti@gmail.com>
2023-11-23 16:06:19 -0300

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -1,19 +1,11 @@
 'use strict';
 
 // -----------------CONSTANTS-----------------
+// -------------------------------------------
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-const openModal = function (e) {
-  e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const header = document.querySelector('.header');
@@ -27,13 +19,30 @@ const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 const allButtons = document.getElementsByTagName('button');
 const link = document.querySelector('.nav__link--btn');
+const navHeight = nav.getBoundingClientRect().height;
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+const maxSlide = slides.length;
+
+// -----------------FUNCTIONS-----------------
+// -------------------------------------------
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 const alertH1 = (e) => {
   alert('addEventListener: Great! You are reading the heading!');
   // h1.removeEventListener('mouseenter', alertH1);
 };
 const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
-// Menu fade animation
+//-----------------MENU FADE ANIMATION-----------------
 const handleHover = function (e) {
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
@@ -45,7 +54,6 @@ const handleHover = function (e) {
     logo.style.opacity = this;
   }
 };
-const navHeight = nav.getBoundingClientRect().height;
 const stickyNav = function (entries) {
   const [entry] = entries;
   console.log(entry);
@@ -68,17 +76,14 @@ const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
   threshold: 0.15,
 });
-// Lazy loading images
+// -----------------LAZY LOADING IMAGES-----------------
 const imgTargets = document.querySelectorAll('img[data-src]');
 const loadImg = function (entries, observer) {
   const [entry] = entries;
   console.log('entry:', entry);
-
   if (!entry.isIntersecting) return;
-
   // Replace src with data-src
   entry.target.src = entry.target.dataset.src;
-
   entry.target.addEventListener('load', function () {
     entry.target.classList.remove('lazy-img');
   });
@@ -90,11 +95,7 @@ const imgObserver = new IntersectionObserver(loadImg, {
   threshold: 0,
   rootMargin: '200px',
 });
-// Slider
-const slides = document.querySelectorAll('.slide');
-const btnLeft = document.querySelector('.slider__btn--left');
-const btnRight = document.querySelector('.slider__btn--right');
-const maxSlide = slides.length;
+// -----------------SLIDER-----------------
 // 0%, 100%, 200%, 300%
 const goToslide = function (slide) {
   slides.forEach((s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`));
@@ -119,13 +120,14 @@ const prevslide = function () {
   goToslide(curSlide);
   activateDot(curSlide);
 };
+// Dot container
 const dotContainer = document.querySelector('.dots');
 const createDots = function () {
   slides.forEach(function (_, index) {
     dotContainer.insertAdjacentHTML('beforeend', `<button class="dots__dot" data-slide="${index}"></button>`);
   });
 };
-
+// Activate the dot
 const activateDot = function (slide) {
   document.querySelectorAll('.dots__dot').forEach((dot) => dot.classList.remove('dots__dot--active'));
   document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active');
@@ -177,7 +179,7 @@ document.querySelector('.btn--close--cookie').addEventListener('click', function
   message.parentElement.removeChild(message);
 });
 
-// Styles
+// -----------------STYLES-----------------
 message.style.backgroundColor = '#37383d';
 message.style.width = '100%';
 
@@ -194,32 +196,29 @@ message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) +
 // setProperty: changes the style of the property (property name, to)
 document.documentElement.style.setProperty('--color-primary', 'rgb(156,218,147)');
 
-// Attributes (elements that go into the tags <>)
+// -----------------ATTRIBUTES (elements that go into the tags <>)-----------------
 console.log('logo.src:', logo.src);
 console.log('logo.className:', logo.className);
 
-// Non-standard attributes
+// -----------------NON-STANDARD ATTRIBUTES-----------------
 console.log(logo.designer); //results in nothing, therefore the 'getAttribute' string must be used
 console.log(logo.getAttribute('designer'));
 logo.setAttribute('company', 'Bankist');
 console.log(logo.src);
 console.log(logo.getAttribute('src'));
-
 console.log(link.href); //shows the link as it is
 console.log(link.getAttribute('href')); //shows the link as it was written in the 'href' element in the HTML file
 
-// Data attributes
+// -----------------DATA ATTRIBUTES-----------------
 console.log(logo.dataset.versionNumber); //data-version-number from index.html (it has to be 'translated' as camelCase)
 
-// Classes
+// -----------------CLASSES-----------------
 logo.classList.add('c', 'j');
 logo.classList.remove('c', 'j');
 logo.classList.toggle('c');
 logo.classList.contains('c'); // not includes
 
-///////////////////////////////////////
-// Button scrolling
-
+// -----------------BUTTON SCROLLING-----------------
 // getBoundingClientRect → proportional to the size of the viewport
 btnScrollTo.addEventListener('click', (e) => {
   const s1coords = section1.getBoundingClientRect();
@@ -240,8 +239,7 @@ btnScrollTo.addEventListener('click', (e) => {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-///////////////////////////////////////
-// Page navigation
+// -----------------PAGE NAVIGATION-----------------
 // document.querySelectorAll('.nav__link').forEach(function (el) {
 //   el.addEventListener('click', function (e) {
 //     e.preventDefault();
@@ -379,10 +377,6 @@ allSections.forEach(function (section) {
 imgTargets.forEach((img) => imgObserver.observe(img));
 
 let curSlide = 0;
-
-goToslide(0);
-goToslide(curSlide);
-
 btnRight.addEventListener('click', nextSlide);
 btnLeft.addEventListener('click', prevslide);
 
@@ -390,8 +384,6 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'ArrowLeft') prevslide();
   e.key === 'ArrowRight' && nextSlide();
 });
-
-activateDot(0);
 
 dotContainer.addEventListener('click', function (e) {
   if (e.target.classList.contains('dots__dot')) {
