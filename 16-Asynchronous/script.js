d874eb24c1cf1fef5f15b22085895a96d3153212
Maurício <93089055+itoalberti@users.noreply.github.com>
2024-08-19 16:56:08 -0300

    Updates (function getJSON)

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -10,10 +10,7 @@ const renderError = function (msg) {
 
 const renderCountry = function (data, className = '') {
   const languages = Object.values(data.languages)[0];
-  console.log(`languages:`, languages);
   const currencies = Object.values(data.currencies)[0].name;
-  console.log(`currencies:`, currencies);
-  //
   const html = `
   <article class="country ${className}">
     <img class="country__img" src="${data.flags.svg}" />
@@ -30,27 +27,35 @@ const renderCountry = function (data, className = '') {
   counstriesContainer.style.opacity = 1;
 };
 
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  // 'fetch' returns a promise, therefore the need for the 'then' functions
+  return fetch(url).then((resp) => {
+    if (!resp.ok) throw new Error(`${errorMsg}: Error ${resp.status}`);
+    return resp.json();
+  });
+};
+
 const getCountryData = function (country) {
-  // Fetch returns a promise, therefore the need for the 'then' functions
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then((resp) => resp.json())
+  // COUNTRY 1
+  getJSON(`https://restcountries.com/v3.1/name/${country}`, `Country not found`)
     .then((data) => {
+      console.log('Main country:');
+
       renderCountry(data[0]);
       const neighbour = data[0].borders[0];
-      console.log('neighbour:', neighbour);
-      if (!neighbour) return;
+      console.log('Neighbour:');
+      if (!neighbour) throw new Error(`No neighbour found!`);
 
-      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+      // COUNTRY 2
+      return getJSON(`https://restcountries.com/v3.1/alpha/${neighbour}`, `Neighbour country not found`);
     })
-    .then((resp) => resp.json())
     .then((data) => {
-      console.log('data2:', data);
       renderCountry(data[0], 'neighbour');
     })
     // catch: catches any errors that occur in the chain
     .catch((err) => {
-      // alert(err),
-      console.error(err), renderError(`Something went wrong: ${err.message}. Make the necessary corrections and try again`);
+      console.error(err);
+      renderError(`${err.message}. Make the necessary corrections and try again.`);
     })
     // finally: is always called at the end of the promise, no matter its result
     .finally(() => {});
@@ -91,5 +96,5 @@ const getCountryData = function (country) {
 // req.send();
 
 btn.addEventListener('click', function () {
-  getCountryData(`austria`);
+  getCountryData(`brazil`);
 });
